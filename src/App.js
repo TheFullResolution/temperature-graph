211faa1ddd52068fff39bdc97220d83ca03625f7
Jedrzej Lewandowski <progreacja@gmail.com>
2018-01-31 20:53:35 +0100

    Set up config

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import * as style from './App.scss'
+
 import React, { Component } from 'react';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className={style.header}>Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
