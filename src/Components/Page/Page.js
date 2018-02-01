ae3833b40b66e29908d381b0f33e80f52d9e5cd6
Jedrzej Lewandowski <progreacja@gmail.com>
2018-02-01 23:11:28 +0100

    Add Chart.js

--- src/Components/Page/Page.js
+++ src/Components/Page/Page.js
@@ -2,6 +2,34 @@ import * as style from './Page.scss'
 
 import React from 'react'
 import { number } from 'prop-types'
+import { Line } from 'react-chartjs-2'
+
+const data = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+    {
+      label: 'My First dataset',
+      fill: false,
+      lineTension: 0.1,
+      backgroundColor: 'rgba(75,192,192,0.4)',
+      borderColor: 'rgba(75,192,192,1)',
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: 'rgba(75,192,192,1)',
+      pointBackgroundColor: '#fff',
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      data: [65, 59, 80, 81, 56, 55, 40]
+    }
+  ]
+}
 
 export const Page = ({ currentTemp }) => (
   <div className={style.container}>
@@ -9,6 +37,7 @@ export const Page = ({ currentTemp }) => (
       <h1 className={style.header}>Temprature Graph App</h1>
     </header>
     <p>Berlin Temprature: {currentTemp}</p>
+    <Line data={data} />
   </div>
 )
 
